92cbdab43952184a3fce17c3b017ab102dc94e16
EmilioVP <emiliovpsis@gmail.com>
2025-10-27 18:31:35 -0600

    Cambios en test_mail

--- test_mail.php
+++ test_mail.php
@@ -48,7 +48,7 @@ $mail->SMTPAuth = true;
 $mail->Username = '22030918@itcelaya.edu.mx';
 
 //Password to use for SMTP authentication
-$mail->Password = 'fyxttvvyyfvilcnl';
+$mail->Password = '';
 
 //Set who the message is to be sent from
 //Note that with gmail you can only use your account address (same as `Username`)
